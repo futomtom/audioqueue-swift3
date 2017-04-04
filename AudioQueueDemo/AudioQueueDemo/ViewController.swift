ea00d1fe7c4ec263329a46195c6b39334d868ca6
Alex <afu@dynosense.com>
2017-04-04 11:55:59 -0700

    fix button label

--- AudioQueueDemo/AudioQueueDemo/ViewController.swift
+++ AudioQueueDemo/AudioQueueDemo/ViewController.swift
@@ -5,16 +5,20 @@ class ViewController: UIViewController {
     var playing = false
     var player: AudioPlayer?
 
+    @IBOutlet weak var playButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        player = AudioPlayer(url: URL(string: "https://drive.google.com/uc?export=download&id=0B8CZdQgUg03peWp5c0pQelNmNFU")!)
+        player = AudioPlayer(url: URL(string: "https://rawgit.com/futomtom/audioqueue-swift3/master/sample.mp3")!)
     
     }
 
     @IBAction func playDidTapped(_ sender: UIButton) {
         let _ = playing ?  player?.pause() : player?.play()
         playing = !playing
+        let title = playing ? "pause": "play"
+        
+        playButton.setTitle(title, for: .normal)
     }
 }
 
